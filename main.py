3497cec3aa492c00890c4c67ab743d96648015e0
Jfrosty <jfrostybeat@gmail.com>
2024-12-16 23:30:24 -0500

    Ch 2: Player Modules complete

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pygame
+from constants import *
 print("Starting asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
+
+def main():
+
+    pass
+
 if __name__ == "__main__":
     main()
