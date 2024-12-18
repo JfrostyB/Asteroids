d974ae2a860a90e4e94535d0d03e9ac663a31409
Jfrosty <jfrostybeat@gmail.com>
2024-12-18 00:05:30 -0500

    Ch 2: Player Game Loop complete

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    pygame.display.flip()
 
-    pass
+    
 
 if __name__ == "__main__":
     main()
