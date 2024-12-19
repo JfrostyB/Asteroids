ffb94a9a83300f78d93594aba301f90570673b4c
Jfrosty <jfrostybeat@gmail.com>
2024-12-18 23:58:55 -0500

    Ch 2: Player FPS complete

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.display.flip()
-
+                
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
